1cbde9612e6570fca861f016c0f04fc7e708ac34
Mrkotadiya <89649335+Mrkotadiya@users.noreply.github.com>
2022-02-11 15:26:06 +0530

    11-02-2022

---
+++ FIrst.js
@@ -0,0 +1,9 @@
+let a = [5,4,3,2,1]
+console.log(a.reverse());
+
+// function reverse_a_number(n)
+// {
+// 	n = n + "";
+// 	return n.reverse();
+// }
+// console.log(Number(reverse_a_number(12345)));
